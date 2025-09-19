f55e6ddcb87ebc5a3e8fbee43ccdc3861074e524
Caleb Thomas <scalny2211@ueab.ac.ke>
2025-09-18 21:54:29 -0700

    host

--- motraylev/settings.py
+++ motraylev/settings.py
@@ -35,8 +35,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 # Application definition
 
