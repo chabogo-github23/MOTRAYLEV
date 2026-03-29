20b1549777e2e55c2a0d8108dbc761928e72db15
Caleb Thomas <139891468+chabogo-github23@users.noreply.github.com>
2026-03-29 17:58:01 +0300

    Comment out DATABASES default configuration
    Comment out the DATABASES default configuration.

--- motraylev/settings.py
+++ motraylev/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
     }
 }
 
-DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
+#DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
